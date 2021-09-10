339d32325f47803f484ec64999b32d23875f864e
Matt Burns <mmburns88@gmail.com>
2021-09-10 11:18:57 -0400

    added variables to CanvasManager

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DECX.UIManager;
 using TMPro;
 
 public class CanvasManager : MonoBehaviour
 {
+    public Button drawCardButton;
     public TMP_Text errorMessage;
+
+    [Header("Player Data")] 
+    public TMP_Text energyPointsDisplay;
+
+    [Header("Images")] 
+    public Image energyPointsBackgroundImage;
+    public Texture energyPointsBackgroundIcon;
 
     private void Awake()
     {
